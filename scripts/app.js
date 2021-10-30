f4713482b50ee5805d3cc6cbcc17d50aa6d998b3
Edwin Mwangi <edwinmwangi138@gmail.com>
2021-10-30 23:06:42 +0300

    displaying chats in UI

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,17 @@
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+
+newChatForm.addEventListener('submit', e =>{
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+        .then(() => newChatForm.reset())
+        .catch(err => console.log(err));
+});
 
 //class instances
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom('gaming','shaun');
 //get chats and render to DOM
-chatroom.getChats(data => chatUI.render(data));
+chatroom.getChats(data => chatUI.render(data)); 
+//no problem if chatroom is after the event listener and used in it coz code aleady read and submit event is initiated by user
